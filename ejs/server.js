56ed1aeb9287be0b0371922f2f233440f173aece
Agustin Citro <dellacquahidroponia@gmail.com>
2022-07-29 19:54:52 -0400

    commit incluyendo tps

---
+++ ejs/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 8080;
+
+const productos = [
+    { title: 'Lechuga Hidroponica', price: 250, thumbnail: 'https://cdn4.iconfinder.com/data/icons/food-4-9/128/food_Cabbage-Vegetable-Organic-Lettuce-256.png', id: 1 },
+    { title: 'Rucula Hidroponica', price: 450, thumbnail: 'https://cdn3.iconfinder.com/data/icons/arugula-1/500/vab1055_1_arugula_isometric-128.png', id: 2 },
+    { title: 'Tomatitos Cherry', price: 500, thumbnail: 'https://cdn2.iconfinder.com/data/icons/fruits-vegetables-2/83/cherry_tomato-128.png', id: 3 },
+  ];
+
+app.set('view engine', 'ejs');
+
+const server = app.listen(PORT, () => {
+  console.log(`Server corriendo en puerto ${PORT}`);
+});
+
+app.post('/productos', (req, res) => {
+  const id = productos.length !== 0 ? productos[productos.length - 1].id + 1 : 1;
+  const productoIngresado = req.body;
+  const productoAGuardar = { ...productoIngresado, id: id };
+  productos.push(productoAGuardar);
+  res.writeHead(301, { Location: '/productos' });
+  return res.end();
+});
+
+app.get('/productos', (req, res) => {
+  res.render('pages/productos', {
+    productos: productos,
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('pages/formulario');
+});
+
+server.on('error', (error) => console.log(`Error en el servidor: ${error}`));
---
+++ handlebars/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const app = express();
+
+const handlebars = require('express-handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 8080;
+
+const productos = [
+    { title: 'Lechuga Hidroponica', price: 250, thumbnail: 'https://cdn4.iconfinder.com/data/icons/food-4-9/128/food_Cabbage-Vegetable-Organic-Lettuce-256.png', id: 1 },
+    { title: 'Rucula Hidroponica', price: 450, thumbnail: 'https://cdn3.iconfinder.com/data/icons/arugula-1/500/vab1055_1_arugula_isometric-128.png', id: 2 },
+    { title: 'Tomatitos Cherry', price: 500, thumbnail: 'https://cdn2.iconfinder.com/data/icons/fruits-vegetables-2/83/cherry_tomato-128.png', id: 3 },
+  ];
+
+const server = app.listen(PORT, () => {
+  console.log(`Server corriendo en puerto ${PORT}`);
+});
+app.engine(
+  'hbs',
+  handlebars.engine({
+    extname: '.hbs',
+    defaultLayout: 'index.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+  })
+);
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.post('/productos', (req, res) => {
+  const id = productos.length !== 0 ? productos[productos.length - 1].id + 1 : 1;
+  const productoIngresado = req.body;
+  const productoAGuardar = { ...productoIngresado, id: id };
+  productos.push(productoAGuardar);
+  res.writeHead(301, { Location: '/productos' });
+  return res.end();
+});
+
+app.get('/', (req, res) => {
+  res.render('formulario');
+});
+
+app.get('/productos', (req, res) => {
+  res.render('productos', {
+    productos: productos,
+  });
+});
+
+server.on('error', (error) => console.log(`Error en el servidor: ${error}`));
